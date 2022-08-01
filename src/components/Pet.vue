88f5e9e747726b83f69981769a916010a4d9d3d9
Soobratty Anas <soobratty.as@gmail.com>
2022-08-01 22:39:38 +0400

    component with primevue

--- src/components/Pet.vue
+++ src/components/Pet.vue
@@ -1,6 +1,24 @@
 <template>
-    <div class="pet">
-        <h3>{{ pet.name }}</h3>
+    <div>
+        <Card style="width: 25em">
+            <template #header>
+                <img src="https://www.primefaces.org/wp-content/uploads/2020/02/primefacesorg-primevue-2020.png"
+                    style="height: 15rem" />
+            </template>
+            <template #title>
+                {{ pet.name }}
+            </template>
+            <template #subtitle>
+                Age: {{ pet.age }}
+            </template>
+            <template #content>
+                <p></p>
+            </template>
+            <template #footer>
+                <Button icon="pi pi-heart" class="p-button-rounded p-button-danger" style="margin-right: 4rem;" />
+                <Button icon="pi pi-times" label="Cancel" class="p-button-secondary" style="margin-left: .5em" />
+            </template>
+        </Card>
     </div>
 </template>
 
